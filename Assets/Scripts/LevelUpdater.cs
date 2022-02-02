7d6790dd75ed2d0c17770f7fbfda360b0c4cfcce
Nathan <sei9i@live.com>
2022-02-01 22:12:47 -0600

    Removed UserSettings directory

---
+++ Assets/Scripts/LevelUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpdater : MonoBehaviour {
+    public LevelManager levelManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+
+
+    void Awake()
+    {
+        Debug.Log("Music player start! Process ID: " + GetInstanceID());
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player being destroyed...");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
